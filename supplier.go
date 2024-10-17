6acbfd0472bd58fc4843d379d7fed802794325a6
Max Reiter <max.reiter@phoenixcollege.edu>
2024-10-17 07:10:14 -0700

    add methods for suppliers

--- supplier.go
+++ supplier.go
@@ -1,1 +1,34 @@
 package marksman
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/maxreiter/marksman/params/supplier"
+	"github.com/maxreiter/marksman/snipeit"
+)
+
+type Suppliers struct {
+	Total int                 `json:"total"`
+	Rows  []*snipeit.Supplier `json:"rows"`
+}
+
+func (c *Client) Suppliers(ctx context.Context, opts ...supplier.RequestOption) (*Suppliers, error) {
+	ro := &supplier.RequestOptions{}
+
+	for _, o := range opts {
+		o(ro)
+	}
+
+	req := request{
+		method: http.MethodGet,
+		url:    "/suppliers",
+	}
+
+	var response *Suppliers
+	if err := c.do(ctx, req, &response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
