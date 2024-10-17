6ac1049110ea001695cbc110240c0ef2a4d7e821
Max Reiter <max.reiter@phoenixcollege.edu>
2024-10-17 07:10:14 -0700

    add methods for fields

--- field.go
+++ field.go
@@ -2,8 +2,10 @@ package marksman
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/maxreiter/marksman/params/field"
 	"github.com/maxreiter/marksman/snipeit"
 )
 
@@ -24,4 +26,111 @@ func (c *Client) Fields(ctx context.Context) (*Fields, error) {
 	}
 
 	return response, nil
+}
+
+func (c *Client) CreateField(ctx context.Context, opts ...field.RequestOption) error {
+	ro := &field.RequestOptions{}
+
+	for _, o := range opts {
+		o(ro)
+	}
+
+	bod, err := ro.Marshal()
+	if err != nil {
+		return err
+	}
+
+	req := request{
+		method: http.MethodPost,
+		url:    "/fields",
+		body:   bod,
+	}
+
+	return c.do(ctx, req, nil)
+}
+
+func (c *Client) Field(ctx context.Context, id snipeit.FieldID) (*snipeit.Field, error) {
+	req := request{
+		method: http.MethodGet,
+		url:    "/fields",
+	}
+
+	var response *snipeit.Field
+	if err := c.do(ctx, req, &response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (c *Client) UpdateField(ctx context.Context, id snipeit.FieldID, opts ...field.RequestOption) error {
+	ro := &field.RequestOptions{}
+
+	for _, o := range opts {
+		o(ro)
+	}
+
+	bod, err := ro.Marshal()
+	if err != nil {
+		return err
+	}
+
+	req := request{
+		method: http.MethodPut,
+		url:    fmt.Sprintf("/fields/%d", id),
+		body:   bod,
+	}
+
+	return c.do(ctx, req, nil)
+}
+
+func (c *Client) DeleteField(ctx context.Context, id snipeit.FieldID) error {
+	req := request{
+		method: http.MethodDelete,
+		url:    fmt.Sprintf("/fields/%d", id),
+	}
+
+	return c.do(ctx, req, nil)
+}
+
+func (c *Client) AssociateField(ctx context.Context, id snipeit.FieldID, opts ...field.RequestOption) error {
+	ro := &field.RequestOptions{}
+
+	for _, o := range opts {
+		o(ro)
+	}
+
+	bod, err := ro.Marshal()
+	if err != nil {
+		return err
+	}
+
+	req := request{
+		method: http.MethodPost,
+		url:    fmt.Sprintf("/fields/%d/associate", id),
+		body:   bod,
+	}
+
+	return c.do(ctx, req, nil)
+}
+
+func (c *Client) DisassociateField(ctx context.Context, id snipeit.FieldID, opts ...field.RequestOption) error {
+	ro := &field.RequestOptions{}
+
+	for _, o := range opts {
+		o(ro)
+	}
+
+	bod, err := ro.Marshal()
+	if err != nil {
+		return err
+	}
+
+	req := request{
+		method: http.MethodPost,
+		url:    fmt.Sprintf("/fields/%d/disassociate", id),
+		body:   bod,
+	}
+
+	return c.do(ctx, req, nil)
 }
