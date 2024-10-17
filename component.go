2336d15fb535ffafc223cfe714d8fda0a090f336
Max Reiter <max.reiter@phoenixcollege.edu>
2024-10-17 07:10:14 -0700

    add methods for components

--- component.go
+++ component.go
@@ -2,6 +2,7 @@ package marksman
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/maxreiter/marksman/params/component"
@@ -54,6 +55,106 @@ func (c *Client) CreateComponent(ctx context.Context, opts ...component.RequestO
 	req := request{
 		method: http.MethodPost,
 		url:    "/components",
+		body:   bod,
+	}
+
+	return c.do(ctx, req, nil)
+}
+
+func (c *Client) Component(ctx context.Context, id snipeit.ComponentID) (*snipeit.Component, error) {
+	req := request{
+		method: http.MethodGet,
+		url:    fmt.Sprintf("/components/%d", id),
+	}
+
+	var response *snipeit.Component
+	if err := c.do(ctx, req, &response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (c *Client) UpdateComponent(ctx context.Context, id snipeit.ComponentID, opts ...component.RequestOption) error {
+	ro := &component.RequestOptions{}
+
+	for _, o := range opts {
+		o(ro)
+	}
+
+	values, err := ro.Values()
+	if err != nil {
+		return err
+	}
+
+	req := request{
+		method: http.MethodPut,
+		url:    fmt.Sprintf("/components/%d", id),
+		query:  values,
+	}
+
+	return c.do(ctx, req, nil)
+}
+
+func (c *Client) DeleteComponent(ctx context.Context, id snipeit.ComponentID) error {
+	req := request{
+		method: http.MethodDelete,
+		url:    fmt.Sprintf("/components/%d", id),
+	}
+
+	return c.do(ctx, req, nil)
+}
+
+func (c *Client) ComponentAssets(ctx context.Context, id snipeit.ComponentID) (*Assets, error) {
+	req := request{
+		method: http.MethodGet,
+		url:    fmt.Sprintf("/components/%d/assets", id),
+	}
+
+	var response *Assets
+	if err := c.do(ctx, req, &response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (c *Client) CheckoutComponent(ctx context.Context, id snipeit.ComponentID, opts ...component.RequestOption) error {
+	ro := &component.RequestOptions{}
+
+	for _, o := range opts {
+		o(ro)
+	}
+
+	bod, err := ro.Marshal()
+	if err != nil {
+		return err
+	}
+
+	req := request{
+		method: http.MethodPost,
+		url:    fmt.Sprintf("/components/%d/checkout", id),
+		body:   bod,
+	}
+
+	return c.do(ctx, req, nil)
+}
+
+func (c *Client) CheckinComponent(ctx context.Context, id snipeit.ComponentID, opts ...component.RequestOption) error {
+	ro := &component.RequestOptions{}
+
+	for _, o := range opts {
+		o(ro)
+	}
+
+	bod, err := ro.Marshal()
+	if err != nil {
+		return err
+	}
+
+	req := request{
+		method: http.MethodPost,
+		url:    fmt.Sprintf("/components/%d/checkin", id),
 		body:   bod,
 	}
 
